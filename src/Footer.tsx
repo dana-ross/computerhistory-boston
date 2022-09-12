a011512d049f0672f25d4933e9b6695cbd4a9c30
Dana Ross <dana@danaross.dev>
2022-09-12 11:46:12 -0400

    Github link (#6)

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
     return (
         <footer>
-            <p>Made with ❤️ in Boston by <a href="https://danaross.dev" target="_blank" rel="noreferrer">Dana Ross</a></p>
+            <p>Made with ❤️ in Boston by <a href="https://danaross.dev" target="_blank" rel="noreferrer">Dana Ross</a> &bull; <a href="https://github.com/dana-ross/computerhistory-boston" target="_blank" rel="noreferrer">GitHub</a></p>
         </footer>
     );
 }
