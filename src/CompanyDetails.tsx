3691887e8086ce388df5bce92ac9cd8fb1c0dce0
Dana Ross <dana@danaross.dev>
2025-01-21 17:14:41 -0500

    Sidebar on CompanyDetails

--- src/CompanyDetails.tsx
+++ src/CompanyDetails.tsx
@@ -11,6 +11,7 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { setMap } from "./MapContext";
 import ReturnToMap from "./ReturnToMap";
 import { Helmet } from "react-helmet";
+import Sidebar from "./Sidebar";
 
 function updateMapContext(map: Leaflet.Map) {
   setMap(map);
@@ -26,51 +27,7 @@ export default function CompanyDetails() {
     return <FourOhFour />;
   } else {
     return (
-      <div className="company-container">
-        <Header className="w-full mb-4" />
-        <div>
-          <main className="ml-4">
-            <LandmarkLogo landmark={companyRecords[0]}></LandmarkLogo>
-            <h1 className="text-xl text-black my-4">{companyRecords[0].name}</h1>
-            <p className="my-4">{companyRecords[0].description}</p>
-            <h2 className="text-lg text-black my-4">Known Locations</h2>
-            <div className="my-4">
-            {companyRecords.map((company) => {
-              return (
-                <div className="bg-slate-200 w-72 p-4">
-                  <div className="w-16 mb-4">
-                    <MapContainer
-                      center={company.location as LatLngTuple}
-                      zoom={12}
-                      whenCreated={updateMapContext}
-                      className="w-64 h-64"
-                    >
-                      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                      <Marker position={company.location} />
-                    </MapContainer>
-                  </div>
-                  <address className="not-italic text-sm">
-                    {company.address.street}
-                    <br />
-                    {company.address.city}, {company.address.state}{" "}
-                    {company.address.zip}
-                  </address>
-                </div>
-              );
-            })}
-            </div>
-          </main>
-          <aside>
-            <LandmarkLogo landmark={companyRecords[0]}></LandmarkLogo>
-            {!!companyRecords[0] && !!companyRecords[0].links.length && (
-              <div className="company-links">
-                <h3>Links:</h3>
-                {companyRecords[0].linksUnorderedList()}
-              </div>
-            )}
-          </aside>
-        </div>
-        <ReturnToMap />
+      <div className="company-container flex">
         <Helmet
           title={` ${companyRecords[0].name} : Boston's Computer History`}
         >
@@ -79,7 +36,55 @@ export default function CompanyDetails() {
             href={`https://www.computerhistory.boston/company/${companyRecords[0].slug}/`}
           />
         </Helmet>
-        <Footer></Footer>
+        <Sidebar />
+        <main>
+          <Header className="w-full mb-4" />
+          <div>
+            <main className="ml-4">
+              <LandmarkLogo landmark={companyRecords[0]}></LandmarkLogo>
+              <h1 className="text-xl text-black my-4">{companyRecords[0].name}</h1>
+              <p className="my-4">{companyRecords[0].description}</p>
+              <h2 className="text-lg text-black my-4">Known Locations</h2>
+              <div className="my-4">
+                {companyRecords.map((company) => {
+                  return (
+                    <div className="bg-slate-200 w-72 p-4">
+                      <div className="w-16 mb-4">
+                        <MapContainer
+                          center={company.location as LatLngTuple}
+                          zoom={12}
+                          whenCreated={updateMapContext}
+                          className="w-64 h-64"
+                        >
+                          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                          <Marker position={company.location} />
+                        </MapContainer>
+                      </div>
+                      <address className="not-italic text-sm">
+                        {company.address.street}
+                        <br />
+                        {company.address.city}, {company.address.state}{" "}
+                        {company.address.zip}
+                      </address>
+                    </div>
+                  );
+                })}
+              </div>
+            </main>
+            <aside>
+              <LandmarkLogo landmark={companyRecords[0]}></LandmarkLogo>
+              {!!companyRecords[0] && !!companyRecords[0].links.length && (
+                <div className="company-links">
+                  <h3>Links:</h3>
+                  {companyRecords[0].linksUnorderedList()}
+                </div>
+              )}
+            </aside>
+          </div>
+          <ReturnToMap />
+          <Footer></Footer>
+        </main>
+
       </div>
     );
   }
