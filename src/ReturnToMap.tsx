8a241c7503d57a139a46a64718e957ba4784067a
Dana Ross <dana@danaross.dev>
2025-01-21 17:06:06 -0500

    Start of sidebar, replacing a with Link, tiny footer

--- src/ReturnToMap.tsx
+++ src/ReturnToMap.tsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function ReturnToMap() {
   return (
     <p className="my-4 ml-4">
-      <a href="/" className="return rounded-full text-white visited:text-white bg-sky-700 py-2 px-4">
+      <Link to="/" className="return rounded-full text-white visited:text-white bg-sky-700 py-2 px-4">
         ⇦ Return to the Map
-      </a>
+      </Link>
     </p>
   );
 }
---
+++ src/Sidebar.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+    return (
+        <menu className="flex-none left-0 top-0 w-20 h-full z-[2000] bg-white text-center text-xs">
+            <ul>
+                <li>
+                    <Link to={"/"} className="block my-4">
+                        <img src="/icons/map.svg" className="w-8 mx-auto" alt="" />
+                        <p>Map</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/companies" className="block my-4">
+                        <img src="/icons/briefcase.svg" className="w-8 mx-auto" alt="" />
+                        <p>Companies</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/institutions" className="block my-4">
+                        <img src="/icons/building-library.svg" className="w-8 mx-auto" alt="" />
+                        <p>Institutions</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/landmarks" className="block my-4">
+                        <img src="/icons/map-pin.svg" className="w-8 mx-auto" alt="" />
+                        <p>Landmarks</p>
+                    </Link>
+                </li>
+            </ul>
+        </menu >
+    );
+}
