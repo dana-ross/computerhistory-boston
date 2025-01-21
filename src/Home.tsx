d2152ed0006192ef028a22239f7e9c5cca54873e
Dana Ross <dana@danaross.dev>
2025-01-21 13:05:32 -0500

    Floating header on map. Style cleanup & fixes. (#14)

--- src/Home.tsx
+++ src/Home.tsx
@@ -13,18 +13,20 @@ export default function Home() {
   return (
     <div className="App">
       <MapContext>
-        <main>
-          <Map></Map>
-        </main>
         <Helmet title="Boston's Computer History">
           <link rel="canonical" href="https://www.computerhistory.boston/" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         </Helmet>
-        <Header className="floating">
+        <Header className="mb-0 pb-4 w-70 absolute top-4 left-2">
           {" "}
           <Search></Search>
         </Header>
-        <Footer />
+        <main>
+          <Map></Map>
+        </main>
+        <div className="w-full fixed bottom-0 left-0 z-[400]">
+          <Footer />
+        </div>
       </MapContext>
     </div>
   );
