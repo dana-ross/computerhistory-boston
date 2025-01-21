d7cbedd49eb11ff4cbc17ee3ac6bb457cbf7e755
Dana Ross <dana@danaross.dev>
2025-01-21 18:10:55 -0500

    Extra padding around Return button

--- src/ReturnToMap.tsx
+++ src/ReturnToMap.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function ReturnToMap() {
   return (
-    <p className="my-4 ml-4">
+    <p className="my-8 ml-4">
       <Link to="/" className="return rounded-full text-white visited:text-white bg-sky-700 py-2 px-4">
         ⇦ Return to the Map
       </Link>
