77705a8270ecf4c776bc79e8a4a76b76492779b1
Dana Ross <dana@danaross.dev>
2025-01-21 22:04:35 -0500

    Index pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CompanyDetails from "./CompanyDetails";
 import ConfigContext from "./ConfigContext";
 import FourOhFour from "./404";
+import CompanyList from "./CompanyList";
+import InstitutionList from "./InstitutionList";
+import LandmarkList from "./LandmarkList";
 
 fixLeafletIcons();
 
@@ -15,6 +18,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/company/:slug" element={<CompanyDetails />} />
+          <Route path="/companies" element={<CompanyList />} />
+          <Route path="/institutions" element={<InstitutionList />} />
+          <Route path="/landmarks" element={<LandmarkList />} />
           <Route path="*" element={<FourOhFour />} />
         </Routes>
       </BrowserRouter>
