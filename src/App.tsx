110ca2578d5a07c0a91275111390cb9481d62567
Dana Ross <dana@danaross.dev>
2025-01-22 07:52:45 -0500

    Institution and Landmark pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import Home from "./Home";
 import fixLeafletIcons from "./fixLeafletIcons";
 import "./App-tw.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CompanyDetails from "./CompanyDetails";
 import ConfigContext from "./ConfigContext";
 import FourOhFour from "./404";
 import CompanyList from "./CompanyList";
 import InstitutionList from "./InstitutionList";
 import LandmarkList from "./LandmarkList";
+import DetailsPage from "./DetailsPage";
 
 fixLeafletIcons();
 
@@ -17,7 +17,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/company/:slug" element={<CompanyDetails />} />
+          <Route path="/company/:slug" element={<DetailsPage />} />
+          <Route path="/institution/:slug" element={<DetailsPage />} />
+          <Route path="/landmark/:slug" element={<DetailsPage />} />
           <Route path="/companies" element={<CompanyList />} />
           <Route path="/institutions" element={<InstitutionList />} />
           <Route path="/landmarks" element={<LandmarkList />} />
