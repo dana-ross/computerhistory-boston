22b98845e7efe3dca4eb5592238c78a4de28b125
Dana Ross <dana@danaross.dev>
2025-01-23 13:46:19 -0500

    Hamburger icon

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,9 +9,12 @@ interface HeaderProps {
 function Header(props: PropsWithChildren<HeaderProps>) {
   return (
     <header id="about" className={props.className || ""}>
-      <Link to="/" className="">
-        <h1 className="font-mono text-lg md:text-2xl text-white [text-shadow:_1px_1px_3px_#116] p-4">Boston's Computer History</h1>
-      </Link>
+      <div className="flex flex-row">
+        <img src="/icons/hamburger-menu.svg" className="w-8 ml-4 mr-2 md:hidden" alt="" />
+        <Link to="/" className="">
+          <h1 className="font-mono text-lg md:text-2xl text-white [text-shadow:_1px_1px_3px_#116] pr-4 md:px-4 py-4">Boston's Computer History</h1>
+        </Link>
+      </div>
       {props.children}
     </header>
   );
